e5fcacdf40d314874ee3f84a017b59ac9e6b6dfa
Solaa08 <solarium_08@hotmail.fr>
2022-09-12 22:12:26 +0200

    add mouseover effect

--- script.js
+++ script.js
@@ -53,3 +53,15 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// mouse over
+
+let test = document.getElementById("example");
+
+test.addEventListener("mouseover", function (event) {
+  event.target.style.color = "red";
+}, false);
+
+test.addEventListener("mouseout", function (event) {
+  event.target.style.color = "black";
+}, false);
